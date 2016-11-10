fd873a6a7e13f71ee877811f7486de5fcf1ffaf4
Mark Eaton <Mark.Eaton@kbcc.cuny.edu>
2016-11-10 15:53:14 -0500

    improved using json.dump

--- 2-py2.py
+++ 2-py2.py
@@ -1,5 +1,6 @@
 # import the libraries that you need
 import requests
+import json
 
 # make a GET request to the OneSearch X-Service API
 response = requests.get('http://onesearch.cuny.edu/PrimoWebServices'
@@ -9,12 +10,9 @@ response = requests.get('http://onesearch.cuny.edu/PrimoWebServices'
 			'&loc=adaptor,primo_central_multiple_fe'
 			'&json=true')
 
-# take the JSON from the response, turn it into a string
-# and store it in a variable called output
-# you need to turn it into a string because a text file can't handle
-# a JSON data structure
-output = str(response.json())
+# take the data from the response, and ignore any non-unicode characters
+output = response.json()
 
 # open a file called myfile.txt and write the output to that file
 with open('myfile.txt', 'w') as file:
-    file.write(output)
+    json.dump(output, file)
--- 3-py2.py
+++ 3-py2.py
@@ -1,5 +1,6 @@
 # import the libraries that you need
 import requests
+import json
 
 # make a GET request to the OneSearch X-Service API
 response = requests.get('http://onesearch.cuny.edu/PrimoWebServices'
@@ -11,9 +12,9 @@ response = requests.get('http://onesearch.cuny.edu/PrimoWebServices'
 
 # take the JSON from the response, and store it in a variable called alldata 
 alldata = response.json()
-data = alldata['SEGMENTS']['JAGROOT']['RESULT']['FACETLIST']['FACET']
-print(data)
+output = alldata['SEGMENTS']['JAGROOT']['RESULT']['FACETLIST']['FACET'][0]['FACET_VALUES']
+print(output)
 
 # open a file called myfile.txt and write the output to that file
 with open('mydata.txt', 'w') as file:
-    file.write(output)
+   json.dump(output, file) 
